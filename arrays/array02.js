c8c8cc57ed2e32737a7c1791ca9c8fc85cc7dcec
Eduvale007 <eduvale007@gmail.com>
2025-01-29 12:58:44 -0300

    news

--- arrays/array02.js
+++ arrays/array02.js
@@ -1,2 +1,21 @@
-const arr =[4,5,10,20,35,4,5]
 
+let player = prompt("Escolha: pedra, papel ou tesoura")
+
+let answers = ["pedra", "papel", "tesoura"]
+
+
+let  computadorPlayer = Math.floor(Math.random() * 3)
+
+let computadorJogada = answers[computadorPlayer]
+
+if (player === computadorJogada) {
+    console.log("Empate!");
+} else if (
+    (player === "pedra" && computadorJogada === "tesoura") ||
+    (player === "papel" && computadorJogada === "pedra") ||
+    (player === "tesoura" && computadorJogada === "papel")
+) {
+    console.log("Você ganhou!");
+} else {
+    console.log("Você perdeu!");
+}
